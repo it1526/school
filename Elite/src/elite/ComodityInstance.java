817d19ef6b11180fad20e9343fda252b2c3f6f01
student-vyt2.12 <vyt2.12@sspu-opava.cz>
2017-11-14 09:19:04 +0100

    Elite

---
+++ Elite/src/elite/ComodityInstance.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package elite;
+
+/**
+ *
+ * @author student
+ */
+public class ComodityInstance {
+    private Comodity comodity;
+    private int buyPrice;
+    private int sellPrice;
+    private int suplyDemand;
+}
---
+++ Elite/src/elite/Ship.java
@@ -0,0 +1,13 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package elite;
+
+public class Ship {
+    private String name;
+    private String comment;
+    private StarPort location;
+    
+}
